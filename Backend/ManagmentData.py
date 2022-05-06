d04b887cb9f6b13fc20cf07f9822c4bcb2ae89a2
Roberto <nuevoz9c@gmail.com>
2022-05-05 19:29:03 -0600

    creacion pdf

--- Backend/ManagmentData.py
+++ Backend/ManagmentData.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, request,jsonify
 from flask_cors import CORS
 from Lectura import LecturaDatos,ArchivoSalida,data,LecturaMensaje,ArchivoSalida2
-from Operaciones import limpiar,limpiar1
+from Operaciones import limpiar,limpiar1,GenerarGrafo,genrarPDFechaEmpresa,ResumenEmpresafecha
 import xml.etree.ElementTree as ET
 import json
 import pdfkit
@@ -11,17 +11,44 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origin": "*"}})
 #CORS (app)
 
+@app.route('/Resumen_Fecha/eventsCleaner/', methods=['GET'])
+def eventsCleaner3():
+    limpiar1()
+    return Response(response='Limpiar')
+
+@app.route ("/resume_FechaGrafo/", methods=['POST'])
+def resume_FechaGrafo():
+    fecha = request.json['date']
+    empresa = request.json['empresa']
+    GenerarGrafo(fecha,empresa)
+    response =  "Se genero Grafo"
+    respuesta = jsonify ({"error": False, "mensaje": response})
+    return (respuesta)
+
+@app.route ("/resume_FechaPDF/", methods=['POST'])
+def resume_FechaPDF():
+    fecha = request.json['date']
+    empresa = request.json['empresa']
+    genrarPDFechaEmpresa(fecha,empresa)
+    response =  "Se genero PDF"
+    respuesta = jsonify ({"error": False, "mensaje": response})
+    return (respuesta)
+
+@app.route ("/resume_Fecha/", methods=['POST'])
+def resume_Fecha():
+    fecha = request.json['date']
+    empresa = request.json['empresa']
+    response =  ResumenEmpresafecha(fecha,empresa)
+    respuesta = jsonify ({"error": False, "mensaje": response})
+    return (respuesta)
+
+
 
 @app.route('/eventsCleaner/', methods=['GET'])
 def eventsCleaner():
     limpiar()
     return Response(response='Limpiar')
 
-@app.route('/shorty/eventsCleaner/', methods=['GET'])
-def eventsCleaner1():
-    print("prueba2")
-    limpiar1()
-    return Response(response='Limpiar')
 
 @app.route('/events/', methods=['POST'])
 def post_events():
@@ -35,16 +62,13 @@ def post_events():
 
     return Response(response='Confirmado xml entrada')
 
-@app.route('/shorty/events/', methods=['POST'])
-def post_events1():
-    dataRquestesd1 = request.data.decode('iso-8859-1')
-    
-    data1 = open('mensaje.xml', 'w+')
-    data1.write(dataRquestesd1)
-    data1.close()
-    LecturaMensaje()
 
-    return Response(response='Confirmado xml mensaje')
+@app.route('/events/', methods=['GET'])
+def get_events():
+    data()
+    dataa = ArchivoSalida()
+    return Response(response=dataa)
+
     
 '''
 @app.route('/eventsPDF/', methods=['GET'])
@@ -55,11 +79,23 @@ def get_eventsPDF():
     return Response(response='Se genero PDF')   
 '''
 
-@app.route('/events/', methods=['GET'])
-def get_events():
-    data()
-    dataa = ArchivoSalida()
-    return Response(response=dataa)
+
+@app.route('/shorty/events/', methods=['POST'])
+def post_events1():
+    dataRquestesd1 = request.data.decode('iso-8859-1')
+    
+    data1 = open('mensaje.xml', 'w+')
+    data1.write(dataRquestesd1)
+    data1.close()
+    LecturaMensaje()
+
+    return Response(response='Confirmado xml mensaje')
+
+@app.route('/shorty/eventsCleaner/', methods=['GET'])
+def eventsCleaner1():
+    print("prueba2")
+    limpiar1()
+    return Response(response='Limpiar')
 
 @app.route('/shorty/events/', methods=['GET'])
 def get_events2():
--- Backend/Operaciones.py
+++ Backend/Operaciones.py
@@ -1,5 +1,11 @@
 import Lectura as LD
+import re
+import pandas as pd
+from tabla import tablas
+import webbrowser
+import datetime
 
+import matplotlib.pyplot as plt
 
 def limpiar():
     LD.ob_men[:]=[]
@@ -16,3 +22,184 @@ def limpiar():
 def limpiar1():
     LD.mensaje_corto[:]=[]
     print(LD.mensaje_corto,"Arreglo4")
+
+
+
+
+def GenerarGrafo(fecha,empresa):
+    try: 
+        global htmlInicial
+        global htmlFinal
+
+        html = ""
+        fechaNew = LecturafechaEntrada(fecha)
+        fechaNew = fechaNew.replace(" ","")
+        empresa = empresa.replace(" ","").lower()
+
+        total_empresa= 0
+        total_positivo = 0
+        total_negativo = 0
+        total_neutro = 0
+
+        validacion = False
+        
+        for i in LD.cantidad_servicio:
+            if i.fecha == fechaNew:
+                if empresa == i.empre:
+                    total_empresa += i.total
+                    total_positivo += i.t_positivo
+                    total_negativo += i.t_negativo
+                    total_neutro += i.t_neutro
+                    validacion = True
+                elif empresa == 'todas':
+                    for x in LD.cantidad_servicio:
+                        if x.fecha == fechaNew:
+                            total_empresa += x.total
+                            total_positivo += x.t_positivo
+                            total_negativo += x.t_negativo
+                            total_neutro += x.t_neutro
+                    validacion = True
+                break
+        
+    
+        eje_x = ['Total:','Total Positivo','Total Negativo','Total Neutro']
+        eje_y = [total_empresa,total_positivo,total_negativo,total_neutro]
+            
+
+        plt.bar(eje_x, eje_y)
+        
+
+        plt.ylabel('Cantidad de Mensajes')
+        
+    
+        plt.xlabel('Empresa ' + str(empresa))
+        
+
+        plt.title('Resumen de fechas del: ' + fechaNew )
+        plt.savefig('./documentacion/ResumenEmpresaFecha.png')
+        plt.close()
+        '''escribir = "<center><h6 class=\"titulos\" ><b> Reporte de resumen de cuentas </b></h6> <br> <img src='ResumenEmpresaFecha.png'>"
+        html += htmlInicial + escribir + htmlFinal
+
+    
+        
+        FileHTML=open("ResumenEmpresaFecha.html","w") 
+        FileHTML.write(html) 
+        FileHTML.close() 
+        path = 'ResumenEmpresaFecha.html'
+        webbrowser.open_new(path)'''
+      
+
+    except:
+        print("La creación del Reporte falló")
+    else:
+         print("Se ha creado el Reporte Token" )
+
+
+def genrarPDFechaEmpresa(fecha,empresa):
+    fechaNew = LecturafechaEntrada(fecha)
+    fechaNew = fechaNew.replace(" ","")
+    empresa = empresa.replace(" ","")
+
+    total_empresa= 0
+    total_positivo = 0
+    total_negativo = 0
+    total_neutro = 0
+
+    validacion = False
+    
+    for i in LD.cantidad_servicio:
+        if i.fecha == fechaNew:
+            if empresa == i.empre:
+                total_empresa += i.total
+                total_positivo += i.t_positivo
+                total_negativo += i.t_negativo
+                total_neutro += i.t_neutro
+                validacion = True
+            elif empresa == 'todas':
+                for x in LD.cantidad_servicio:
+                    if x.fecha == fechaNew:
+                        total_empresa += x.total
+                        total_positivo += x.t_positivo
+                        total_negativo += x.t_negativo
+                        total_neutro += x.t_neutro
+                        validacion = True
+            break
+    data = []
+    Fecha = []
+    Empresa = []
+    valores = []
+    if validacion:
+        Fecha.append("Fecha: " + fechaNew)
+        Empresa.append("Empresa: " + str(empresa))
+        valores.append("Total de Mensajes: " + str(total_empresa))
+        valores.append("Mensajes positivos: " + str(total_positivo))
+        valores.append("Mensajes negativos: " + str(total_negativo))
+        valores.append("Mensajes neutros: " + str(total_neutro))
+        
+    else:
+        valores.append("No se encontro Fecha o Empresa indicada")
+    
+    data.append(Fecha)
+    data.append(Empresa)
+    data.append(valores)
+    tablas(data,'ResumenPorFecha_Empresa.pdf')
+    path = 'ResumenPorFecha_Empresa.pdf'
+    webbrowser.open_new(path)
+
+def ResumenEmpresafecha(fecha,empresa):
+    fechaNew = LecturafechaEntrada(fecha)
+    fechaNew = fechaNew.replace(" ","")
+    empresa = empresa.replace(" ","")
+
+    total_empresa= 0
+    total_positivo = 0
+    total_negativo = 0
+    total_neutro = 0
+
+    validacion = False
+    
+    for i in LD.cantidad_servicio:
+        if i.fecha == fechaNew:
+            if empresa == i.empre:
+                total_empresa += i.total
+                total_positivo += i.t_positivo
+                total_negativo += i.t_negativo
+                total_neutro += i.t_neutro
+                validacion = True
+            elif empresa == 'todas':
+                for x in LD.cantidad_servicio:
+                    if x.fecha == fechaNew:
+                        total_empresa += x.total
+                        total_positivo += x.t_positivo
+                        total_negativo += x.t_negativo
+                        total_neutro += x.t_neutro
+                        validacion = True
+            break
+            
+    
+    if validacion:
+        text = "Fecha: " + str(fechaNew) + "\nEmpresa: " + str(empresa) + "\nTotal Empresa: " + str(total_empresa)+ "\nMensajes Positivos: " + str(total_positivo)+ "\nMensajes Negativos: " + str(total_negativo)+ "\nMensajes Neutros: " + str(total_neutro)
+    else:
+        text = "No se encontro Fecha o Empresa indicada"
+    
+   
+    return text
+
+
+def LecturafechaEntrada(fecha):
+
+    try:
+        fechas = ""
+        arreglo = fecha.split("-")
+        arreglo.reverse()
+        for i in arreglo:
+            fechas += i
+            if len(i) == 4:
+               pass
+            else:
+                fechas += "/"
+        return fechas
+        
+    except:
+        return 'NoSeEncontro'
